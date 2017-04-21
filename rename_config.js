ff2982eb0ab104b614c29f5d76c04904491b1a80
willynogs <willynogs@gmail.com>
2017-04-21 11:31:31 -0400

    add rename_config.js and instructions

---
+++ rename_config.js
@@ -0,0 +1,11 @@
+/*
+  this file contains all credentials and variables ** RENAME THIS FILE config.js AND FILL IN YOUR CREDENTIALS **
+*/
+module.exports = {
+  city: 'Columbus, Ohio', /* city used for weather api */
+  quotes_cat: 'famous', /* can be either famous or movies */
+  sn_instance: '', /* name of servicenow instance to use */
+  sn_username: '', /* servicenow username for REST call */
+  sn_password: '', /* servicenow password for REST call [must be set explicitly, NOT your sso password] */
+  sn_user: '' /* we will get information based on this servicenow username */
+}
